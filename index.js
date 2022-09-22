b5af211ec870e5b6d529bcf78ce2e31cabd0632d
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2022-09-22 00:49:13 -0700

    created player instance

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ canvas.width = 1024
 canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Sprite {
+    constructor(position){
+        this.position = position
+    }
+}
+
+const player = new Sprite({
+    x: 0,
+    y: 0
+})
+
+console.log(player)
