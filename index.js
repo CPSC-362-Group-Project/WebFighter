1bedbd39df43ab8f61f496c75c091a2b501252b7
jthomas2234 <jojithomas25@gmail.com>
2022-09-24 01:29:59 -0700

    changed layout minimally, started health bar

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const c = canvas.getContext("2d")
 
 canvas.width = 1024
 canvas.height = 576
+canvas.style = "position: absolute; top: 10px; left: 0px; right: 0px; bottom: 200px; margin: auto; border:4px solid blue";
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
