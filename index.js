4f741bd99acb3c7a60f2c27968049db2ad219f2c
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2022-09-24 12:13:58 -0700

    create two players and gravity

--- index.js
+++ index.js
@@ -6,17 +6,63 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+const gravity = 0.2
 class Sprite {
-    constructor(position){
+    constructor({position, velocity}){
         this.position = position
+        this.velocity = velocity
+        this.height = 150
+    }
+
+    draw(){
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 50, this.height)
+    }
+
+    update(){
+        this.draw()
+        this.position.y += this.velocity.y
+        if (this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y = 0
+        }
+        else{
+            this.velocity.y += gravity
+        }
     }
 }
 
 const player = new Sprite({
-    x: 0,
-    y: 0
+    position: {
+        x: 100,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
 })
 
+const enemy = new Sprite({
+    position: {
+        x: 700,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+//player.draw()
+//enemy.draw()
 console.log(player)
 
-test
+function animate(){
+    window.requestAnimationFrame(animate)
+    c.fillStyle = "black"
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+}
+
+animate()
