46a6d123ec464d9f60ef8a869ae0f43586bd5416
Avtonoma <ethanmarcelino@gmail.com>
2022-09-30 21:44:00 -0700

    implemented simple music controller, can only pause/play

--- index.js
+++ index.js
@@ -61,3 +61,36 @@ document.getElementById("mute").addEventListener('click', function(event) {
     musicPlayer.mute();
   }
 });
+
+// code for the music controller
+document.getElementById("prevSong").addEventListener('click', function(event){
+  // if current song is at least the second song, can go back
+  if(musicPlayer.getPlaylistIndex() > 0)
+  {
+    musicPlayer.previousVideo();
+  }
+});
+
+document.getElementById("playSong").addEventListener('click', function(event){
+  // if music player is paused, play
+  //if(musicPlayer.getPlayerState() == 2)
+  //{
+    musicPlayer.playVideo();
+  //}
+});
+
+document.getElementById("pauseSong").addEventListener('click', function(event){
+  // if the music player is playing, pause
+  //if(musicPlayer.getPlayerState() == 2)
+  //{
+    musicPlayer.pauseVideo();
+  //}
+});
+
+document.getElementById("nextSong").addEventListener('click', function(event){
+  // if current video isn't last in the playlist, go next
+  if(musicPlayer.getPlaylistIndex() < musicPlayer.getPlaylist().length)
+  {
+    musicPlayer.nextVideo();
+  }
+});
