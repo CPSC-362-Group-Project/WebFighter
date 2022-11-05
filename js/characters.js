3d41e030e93610736c62ae7a1e256777c6afb9f0
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2022-11-04 18:19:07 -0700

    added a bunch of new characters

--- js/characters.js
+++ js/characters.js
@@ -1,3 +1,4 @@
+// new character
 let Kenji = {
     position: {
       x: 850,
@@ -59,6 +60,8 @@ let Kenji = {
     }
 }
 
+
+// new character
 let Matt = {
     position: {
       x: 120,
@@ -119,3 +122,191 @@ let Matt = {
     }
 }
 
+
+// new character
+let Riku = {
+    position: {
+      x: 120,
+      y: 100
+    },
+    velocity: {
+      x: 0,
+      y: 0
+    },
+    offset: {
+      x: 0,
+      y: 0
+    },
+    imageSrc: './assets/characters/Martial-Hero3/Sprite/Idle.png',
+    framesMax: 10,
+    scale: 2.5,
+    offset: {
+      x: 215,
+      y: 60
+    },
+    sprites: {
+      idle: {
+        imageSrc: './assets/characters/Martial-Hero3/Sprite/Idle.png',
+        framesMax: 10
+      },
+      run: {
+        imageSrc: './assets/characters/Martial-Hero3/Sprite/Run.png',
+        framesMax: 8
+      },
+      jump: {
+        imageSrc: './assets/characters/Martial-Hero3/Sprite/Going Up.png',
+        framesMax: 3
+      },
+      fall: {
+        imageSrc: './assets/characters/Martial-Hero3/Sprite/Going Down.png',
+        framesMax: 3
+      },
+      attack1: {
+        imageSrc: './assets/characters/Martial-Hero3/Sprite/Attack1.png',
+        framesMax: 7
+      },
+      takeHit: {
+        imageSrc: './assets/characters/Martial-Hero3/Sprite/Take Hit.png',
+        framesMax: 3
+      },
+      death: {
+        imageSrc: './assets/characters/Martial-Hero3/Sprite/Death.png',
+        framesMax: 11
+      }
+    },
+    attackBox: {
+      offset: {
+        x: 10,
+        y: 50
+      },
+      width: 85,
+      height: 50
+    }
+}
+
+
+// new character
+let Ray = {
+    position: {
+      x: 120,
+      y: 100
+    },
+    velocity: {
+      x: 0,
+      y: 0
+    },
+    offset: {
+      x: 0,
+      y: 0
+    },
+    imageSrc: './assets/characters/Fantasy-Warrior/Sprites/Idle.png',
+    framesMax: 10,
+    scale: 2.5,
+    offset: {
+      x: 215,
+      y: 108
+    },
+    sprites: {
+      idle: {
+        imageSrc: './assets/characters/Fantasy-Warrior/Sprites/Idle.png',
+        framesMax: 10
+      },
+      run: {
+        imageSrc: './assets/characters/Fantasy-Warrior/Sprites/Run.png',
+        framesMax: 8
+      },
+      jump: {
+        imageSrc: './assets/characters/Fantasy-Warrior/Sprites/Jump.png',
+        framesMax: 3
+      },
+      fall: {
+        imageSrc: './assets/characters/Fantasy-Warrior/Sprites/Fall.png',
+        framesMax: 3
+      },
+      attack1: {
+        imageSrc: './assets/characters/Fantasy-Warrior/Sprites/Attack1.png',
+        framesMax: 7
+      },
+      takeHit: {
+        imageSrc: './assets/characters/Fantasy-Warrior/Sprites/Take-hit.png',
+        framesMax: 3
+      },
+      death: {
+        imageSrc: './assets/characters/Fantasy-Warrior/Sprites/Death.png',
+        framesMax: 7
+      }
+    },
+    attackBox: {
+      offset: {
+        x: 10,
+        y: 50
+      },
+      width: 105,
+      height: 50
+    }
+}
+
+
+// new character 
+let Pyro = {
+    position: {
+      x: 120,
+      y: 100
+    },
+    velocity: {
+      x: 0,
+      y: 0
+    },
+    offset: {
+      x: 0,
+      y: 0
+    },
+    imageSrc: './assets/characters/Evil-Wizard/Sprites/Idle.png',
+    framesMax: 8,
+    scale: 2.5,
+    offset: {
+      x: 215,
+      y: 110
+    },
+    sprites: {
+      idle: {
+        imageSrc: './assets/characters/Evil-Wizard/Sprites/Idle.png',
+        framesMax: 8
+      },
+      run: {
+        imageSrc: './assets/characters/Evil-Wizard/Sprites/Move.png',
+        framesMax: 8
+      },
+      jump: {
+        imageSrc: './assets/characters/Evil-Wizard/Sprites/Move.png',
+        framesMax: 8
+      },
+      fall: {
+        imageSrc: './assets/characters/Evil-Wizard/Sprites/Idle.png',
+        framesMax: 8
+      },
+      attack1: {
+        imageSrc: './assets/characters/Evil-Wizard/Sprites/Attack.png',
+        framesMax: 8
+      },
+      takeHit: {
+        imageSrc: './assets/characters/Evil-Wizard/Sprites/Take Hit.png',
+        framesMax: 4
+      },
+      death: {
+        imageSrc: './assets/characters/Evil-Wizard/Sprites/Death.png',
+        framesMax: 8
+      }
+    },
+    attackBox: {
+      offset: {
+        x: 10,
+        y: 50
+      },
+      width: 123,
+      height: 50
+    }
+}
+
+
+// new character
